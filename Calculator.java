4b85b9343ecb2de9d4186791ad7543c4128d3def
TibyanSaad <saad.tibyan02@gmail.com>
2026-02-17 14:05:39 +0400

    Add sum and difference functionality

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner (System.in);
+            System.out.println("Enter first number: ");
+            int num1 = scanner.nextInt();
+            System.out.println("Enter second number: ");
+            int num2 = scanner.nextInt();
 
+            int sum = num1+num2;
+            int differernce = num1 - num2;
 
+            System.out.println("Sum: " + sum);
+            System.out.println("differernce: " + differernce);
 
         scanner.close();
     }
