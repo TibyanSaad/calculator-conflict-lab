8263273648a5e8628718c8dd7284920dbbc9af02
kawther <likawther17@gmail.com>
2026-02-17 14:11:23 +0400

    add multplication and division functionality

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner (System.in);
-
+        System.out.println("Enter first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = scanner.nextInt();
+        int product = num1 * num2;
+        int quotiont= num1 /num2;
+        System.out.println("product: " + product);
+        System.out.println("Quotiont: " + quotiont);
 
 
         scanner.close();
