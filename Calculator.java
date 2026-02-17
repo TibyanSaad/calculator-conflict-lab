cb77b943bbc7361dc78a9c53aec1612d28494785
TibyanSaad <saad.tibyan02@gmail.com>
2026-02-17 15:21:44 +0400

    power function added

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first number:");
         int num1 = scanner.nextInt();
         System.out.println("Enter second number:");
         int num2 = scanner.nextInt();
+
         int product = num1 * num2;
-        int quotiont= num1 /num2;
+        int quotient = num1 / num2;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
         System.out.println("product: " + product);
-        System.out.println("Quotiont: " + quotiont);
-
-            int sum = num1+num2;
-            int differernce = num1 - num2;
-
-
-            System.out.println("Sum: " + sum);
-            System.out.println("differernce: " + differernce);
-
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Sum: " + sum);
+        System.out.println("difference: " + difference);
+        System.out.println("Exponent: " + Math.pow(num1,num2));
         scanner.close();
     }
 }
